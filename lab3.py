f836e5794acb514bda69e525dc68432c1bccb05f
Tanya Ivasuyta <tana210795@gmail.com>
2018-12-01 00:57:21 +0200

    Create lab3.py

---
+++ lab3.py
@@ -0,0 +1,12 @@
+# Python Лабораторна робота № 3. Мариневич Тетяна, FI-9119
+print ( " Python. Лабораторна робота № 3 " )
+print ( ' Мариневич Тетяна, FI-9119 ' )
+from math import*
+print('Введіть значення:')
+a=float(input('x = '))
+b=float(input('z = '))
+if not(exp**(z-1)) or not(abs(x-z)):
+    print('Значення змінних виходять за область визначення функції')
+else:
+    f=(x+exp**(z-1))/(1-x** 2 abs(x-z))
+    print('f =',f)
